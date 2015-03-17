5260a5c5d2359dbb33061b02644b1ff879498e41
Taylor <tsg9292@gmail.com>
2015-03-16 21:13:47 -0600

    yaya

--- twitter_streamer.py
+++ twitter_streamer.py
@@ -3,6 +3,7 @@ from tweepy import OAuthHandler
 from tweepy import Stream
 
 import json
+import os
 
 #Variables that contains the user credentials to access Twitter API
 access_token = "156509990-2OENzM4s6crQlUI9V6geS54tGEN0pEQVQ7Pvb2Xr"
@@ -10,12 +11,20 @@ access_token_secret = "szk6SWcmyRSSW5ECQRKyFyFkERj62cU1WRdydPSdMP8EU"
 consumer_key = "FvHpVtiweeZ1Db4Q89AtXYmHE"
 consumer_secret = "vMZ0Ek28yv3jcnjVh4GDwMvAwPbyuT0VsA5qJNAYxDzP8iirnX"
 
-language_dict={}
-
 #This is a basic listener that just prints received tweets stdout
 class StdOutListener(StreamListener):
 
 	def on_data(self, data):
+		if os.path.isfile('meta_tweets'):
+			with open('meta_tweets') as f:
+				line = f.read()
+				tweet_count,fsize = line.split(',')
+				tweet_count = int(tweet_count)
+				fsize = int(fsize)
+		else:
+			tweet_count = 0
+			fsize = 0
+
 		new_data = {}
 		json_data = json.loads(data)
 		if 'geo' not in data:
@@ -23,21 +32,32 @@ class StdOutListener(StreamListener):
 		if json_data['coordinates']:
 			if json_data['lang'] == 'en':
 				#TODO fix quotes here. Need to all be double quotes except for in the text field
-				new_data = "{'created_at':"
-				new_data = new_data + json.dumps(json_data['created_at'],separators=(',',':')) + ','
-				new_data = new_data + "'id':" + json.dumps(json_data['id'],separators=(',',':')) + ','
-				new_data = new_data + "'text':" + json.dumps(json_data['text'],separators=(',',':')) + ','
-				new_data = new_data + "'coordinates':" + json.dumps(json_data['coordinates'],separators=(',',':')) + ','
-				new_data = new_data + "'place':" + json.dumps(json_data['place'],separators=(',',':')) + ','
-				new_data = new_data + "'entities':" + json.dumps(json_data['entities'],separators=(',',':')) + ','
-				new_data = new_data + "'user':{'id':" + json.dumps(json_data['user']['id'],separators=(',',':')) + '}'
-				new_data = new_data + "}"
+				new_data = "{\"created_at\":" \
+					+ json.dumps(json_data['created_at'],separators=(',',':')) + ',' \
+					+ "\"id\":" + json.dumps(json_data['id'],separators=(',',':')) + ',' \
+					+ "\"text\":" + json.dumps(json_data['text'],separators=(',',':')) + ',' \
+					+ "\"coordinates\":" + json.dumps(json_data['coordinates'],separators=(',',':')) + ',' \
+					+ "\"place\":" + json.dumps(json_data['place'],separators=(',',':')) + ',' \
+					+ "\"entities\":" + json.dumps(json_data['entities'],separators=(',',':')) + ',' \
+					+ "\"user\":{\"id\":" + json.dumps(json_data['user']['id'],separators=(',',':')) + '}' \
+				+ "}"
 
-		print
-		print new_data
-		#if new_data != {}:
-		#		print json.loads(new_data)
+		if new_data != {}:
+			f2 = open('json_tweets','a')
+			if fsize%1000 <= 25 and fsize%1000 >=1:
+				print '===== Number of Tweets: {0}  File size: {1} ====='.format(tweet_count, fsize)
 
+			# if the file size if bigger than ~15Gb, exit
+			if fsize >= 16106127360:
+				return False
+		
+			f2.write(new_data+'\n')
+			tweet_count = tweet_count + 1
+			
+			f2.close()
+			fsize = os.path.getsize('json_tweets')
+			with open('meta_tweets','w') as f1:
+					f1.write('{0},{1}\n'.format(tweet_count,fsize))
 		return True
 
 	def on_error(self, status_code):
